428490f73955634931c61cd1c066555aeea4f966
root <root@pedro-M7X0SU.(none)>
2012-07-17 22:35:03 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SHAREPE::Application.config.secret_token = '1b7eff800ee06e310f2ce566dc647b380be3a8d64ccd7dc429f3ae2eec2ba7212a939d0add7cfd44f2a79b65d8517f39bb5b2f54f09749ed52f7cf09ac14378e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SHAREPE::Application.config.session_store :cookie_store, :key => '_SHAREPE_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SHAREPE::Application.config.session_store :active_record_store
